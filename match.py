681e0f72de9858387d990a066ed2ccc7f696fa56
ElSalamino <86099774+ElSalamino@users.noreply.github.com>
2021-06-18 10:18:26 +0200

    Create match.py
    MatchMaking rudimentale basato sulle posizioni

---
+++ match.py
@@ -0,0 +1,20 @@
+
+def match(sorters,nome):
+    
+    sortes = [a[0] for a in sorters]
+    posizione = sortes.index(nome)
+    
+
+    if posizione < 5:
+        oppo = posizione + random.randint(-posizione,6)
+    
+    else:
+        oppo = posizione + random.randint(-6,6)
+
+    if oppo == posizione:
+        oppo += 1
+    
+    
+    sfidante = sortes[int(oppo)]
+
+    return sfidante
