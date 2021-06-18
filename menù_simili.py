8c2c1541b8242173bc08a921eeaa40d4b3ecd26c
ElSalamino <86099774+ElSalamino@users.noreply.github.com>
2021-06-18 10:37:07 +0200

    Update menù_simili.py

--- menù_simili.py
+++ menù_simili.py
@@ -28,3 +28,22 @@ def approccio(client, message):
     reply_markup = InlineKeyboardMarkup(bottoni)
     
     message.reply_text(txt, reply_markup = reply_markup)
+
+    @app.on_message(filters.command(["wikiboss","wikiboss@NFTlittlebot"]) & ~filters.user(bannati))
+def wiki(client, message): 
+    
+    BossAbi = {"Franco est":"Difficile ma potrebbe valerne la pena","Fantasma del rimorso":"Un fantasma intangibile, che vuoi che sia?","IL FOLLE":"COSE SUCCEDONO MA E' UNO SCONTRO NORMALE E FOOOLLE","Leviatano delle sabbie":"Un leviatano in grado di controllare la sabbia, fate attenzione a non prolungare troppo lo scontro","Cerbero":"Un agglomerato di odio e morte, spesso serve ucciderlo diverse volte prima di morire","Demone spezza-ossa":"Presta attenzione a lui, un colpo solo potrebbe esserti fatale!","Carl":"Non è molto spaventoso più che tutto è molto sfuggevole","Orrore della palude" :"Un tuttuno con la palude, difficile da abbattere senza i giusti mezzi!","IppoSciamano":"Piccolo ed indifeso, piccolo e pieno di antichi fantasmi del passato"}
+    text = "Ecco una wiki dei boss conosciuti:\n"
+    for b in Boss:
+        
+        abi = BossAbi[b]
+        stat = Boss[b]
+        vita = stat["hp"]
+        at = stat["atk"]
+        dif = stat["def"]
+        agi = stat["agi"]
+        
+        text += f"{b}, un terribile boss!\n❤️Vita: {vita}\n🪓Attacco: {at}\n🥋Difesa: {dif}\n🌪️Agilità: {agi}\n⭐️Abilità: {abi}\n\n"
+    text += "Perdere contro un boss ti inabiliterà per molto tempo, fai attenzione!"
+    message.reply(text)
+
